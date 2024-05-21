5d3ae7ef8512e1ee6280500738c9528bae0fa024
DeTuksa <emmydave414@gmail.com>
2024-05-21 20:44:49 +0100

    Improved skills

--- src/app/consts/skills.js
+++ src/app/consts/skills.js
@@ -1,7 +1,7 @@
 export default {
-    language: ["js", "ts", "python"],
-    database: ["sqlite", "postgreSql"],
-    other: ["html", "css", "sass", "scss", "less", "stylus", "ejs", "jinja", "pug"],
-    tool: ["vscode", "nvim", "figma", "git"],
-    framework: ["react", "next", "gulp", "express", "flask", "quart"]   
+    language: ["js", "ts", "python", "rust", "dart", "kotlin"],
+    database: ["sqlite", "postgreSql", "mongo"],
+    other: ["kmm", "rn", "jenkins", "docker", "firebase"],
+    tool: ["vscode", "androidstudio", "git"],
+    framework: ["flutter", "nest", "node", "express", "react"]   
 }
